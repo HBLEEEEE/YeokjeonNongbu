d7d914bedc34060827d7e88d9b53ef029b306965
using6843@gmail.com <using6843@gmail.com>
2024-11-12 17:59:05 +0900

    [feat] 사용자 정의 애니메이션 및 배경 이미지 추가

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -3,7 +3,26 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        intro: "url('./assets/intro/intro.png')"
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { 
+            transform: 'translateY(-100%)',
+            opacity: '0',
+          },
+          '100%': { 
+            transform: 'translateY(0)',
+            opacity: '1', 
+          },
+        },
+      },
+      animation: {
+        slideDown: 'slideDown 1s ease-out forwards',
+      }
+    },
   },
   plugins: []
 };
