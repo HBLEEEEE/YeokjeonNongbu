6f9048f6602d1fc4b1d3e30f32fb6a2e9eeaa9fd
using6843@gmail.com <using6843@gmail.com>
2024-11-12 18:19:27 +0900

    [design] title 하나씩 등장하도록 opacity 추가

--- apps/frontend/src/components/IntroTitle.tsx
+++ apps/frontend/src/components/IntroTitle.tsx
@@ -1,73 +1,76 @@
 const IntroTitle: React.FC = () => {
-    return (
+  return (
+    <div
+      className="
+        flex 
+        absolute 
+        left-[50%] top-[10%] 
+        transform -translate-x-1/2 
+        space-x-4
+        sm:space-x-8 md:space-x-12 lg:space-x-24
+      "
+    >
       <div
         className="
-          flex 
-          absolute 
-          left-[50%] top-[10%] 
-          transform -translate-x-1/2 
-          space-x-4
-          sm:space-x-8 md:space-x-12 lg:space-x-24
+          bg-[url('./assets/intro/yeok.png')]
+          bg-no-repeat 
+          bg-contain
+          w-[60px] h-[100px]
+          sm:w-[80px] sm:h-[120px]
+          md:w-[100px] md:h-[140px]
+          lg:w-[130px] lg:h-[170px]
+          animate-slideDown
         "
-      >
-        <div
-          className="
-            bg-[url('./assets/intro/yeok.png')]
-            bg-no-repeat 
-            bg-contain
-            w-[60px] h-[100px]
-            sm:w-[80px] sm:h-[120px]
-            md:w-[100px] md:h-[140px]
-            lg:w-[130px] lg:h-[170px]
-            animate-slideDown
-          "
-          style={{ animationDelay: '0s' }}
-        ></div>
+        style={{ animationDelay: '0s' }}
+      ></div>
+
+      <div
+        className="
+          bg-[url('./assets/intro/jeon.png')]
+          bg-no-repeat 
+          bg-contain
+          w-[60px] h-[100px]
+          sm:w-[80px] sm:h-[120px]
+          md:w-[100px] md:h-[140px]
+          lg:w-[130px] lg:h-[170px]
+          animate-slideDown
+          opacity-0
+        "
+        style={{ animationDelay: '1s' }}
+      ></div>
+
+      <div
+        className="
+          bg-[url('./assets/intro/nong.png')]
+          bg-no-repeat 
+          bg-contain
+          w-[60px] h-[100px]
+          sm:w-[80px] sm:h-[120px]
+          md:w-[100px] md:h-[140px]
+          lg:w-[130px] lg:h-[170px]
+          animate-slideDown
+          opacity-0
+        "
+        style={{ animationDelay: '2s' }}
+      ></div>
+
+      <div
+        className="
+          bg-[url('./assets/intro/bu.png')]
+          bg-no-repeat 
+          bg-contain
+          w-[40px] h-[100px]
+          sm:w-[60px] sm:h-[120px]
+          md:w-[80px] md:h-[140px]
+          lg:w-[110px] lg:h-[170px]
+          animate-slideDown
+          opacity-0
+        "
+        style={{ animationDelay: '3s' }}
+      ></div>
+    </div>
+  );  
+};
   
-        <div
-          className="
-            bg-[url('./assets/intro/jeon.png')]
-            bg-no-repeat 
-            bg-contain
-            w-[60px] h-[100px]
-            sm:w-[80px] sm:h-[120px]
-            md:w-[100px] md:h-[140px]
-            lg:w-[130px] lg:h-[170px]
-            animate-slideDown
-          "
-          style={{ animationDelay: '1s' }}
-        ></div>
-  
-        <div
-          className="
-            bg-[url('./assets/intro/nong.png')]
-            bg-no-repeat 
-            bg-contain
-            w-[60px] h-[100px]
-            sm:w-[80px] sm:h-[120px]
-            md:w-[100px] md:h-[140px]
-            lg:w-[130px] lg:h-[170px]
-            animate-slideDown
-          "
-          style={{ animationDelay: '2s' }}
-        ></div>
-  
-        <div
-          className="
-            bg-[url('./assets/intro/bu.png')]
-            bg-no-repeat 
-            bg-contain
-            w-[40px] h-[100px]
-            sm:w-[60px] sm:h-[120px]
-            md:w-[80px] md:h-[140px]
-            lg:w-[110px] lg:h-[170px]
-            animate-slideDown
-          "
-          style={{ animationDelay: '3s' }}
-        ></div>
-      </div>
-    );
-  };
-  
-  export default IntroTitle;
+export default IntroTitle;
   
