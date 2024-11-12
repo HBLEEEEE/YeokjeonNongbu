c80fb3f5e64a10889233b7e7cca3a85cb68044eb
DongKey777 <higgs95@naver.com>
2024-11-12 18:47:07 +0900

    [fix] 모듈 의존성 수정

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MarketController } from './market/market.controller';
 import { RedisModule } from './redis/redis.module';
 import { MarketModule } from './market/market.module';
 
 @Module({
   imports: [RedisModule, MarketModule],
-  controllers: [AppController, MarketController],
+  controllers: [AppController],
   providers: [AppService]
 })
 export class AppModule {}
