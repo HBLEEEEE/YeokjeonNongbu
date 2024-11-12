087d68baa931377ed7e2963dd84b9d3057e315a8
DongKey777 <higgs95@naver.com>
2024-11-12 18:28:17 +0900

    [feat] Redis 설치

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MarketController } from './market/market.controller';
+import { RedisModule } from './redis/redis.module';
+import { MarketModule } from './market/market.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [RedisModule, MarketModule],
+  controllers: [AppController, MarketController],
   providers: [AppService]
 })
 export class AppModule {}
