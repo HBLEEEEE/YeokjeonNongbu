c2ecf41d74a92ce0214eed0f72e0270aaef1f986
edder773 <st02045@naver.com>
2024-11-13 19:33:37 +0900

    [feat] 회원가입 기능 추가

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
 import { MarketModule } from './market/market.module';
 import { OrderModule } from './order/order.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RedisModule, MarketModule, OrderModule],
+  imports: [AuthModule, RedisModule, MarketModule, OrderModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService]
 })
