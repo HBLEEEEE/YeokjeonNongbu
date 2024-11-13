03e148e47b7cde6e3d5c1431d1fb91955c83b680
using6843@gmail.com <using6843@gmail.com>
2024-11-13 11:35:15 +0900

    [feat] footer로 잔디 이미지 추가

---
+++ apps/frontend/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer: React.FC = () => {
+  return (
+    <footer
+      className={`
+        fixed
+        bottom-0
+        left-0
+        w-full
+        h-[150px]
+        bg-[url('./assets/public/grass.png')]
+        bg-no-repeat
+        bg-cover
+        opacity-85
+      `}
+    >
+    </footer>
+  );
+};
+
+export default Footer;  
--- apps/frontend/src/pages/Lottery.tsx
+++ apps/frontend/src/pages/Lottery.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
-const Lottery = () => {
+const Lottery: React.FC = () => {
   return (
     <div>
       <Header></Header>
       <h1>복권 페이지</h1>
+      <Footer></Footer>
     </div>
   );
 };
