0b778338c99d19e5911dfa0b17c864a27482d7da
using6843@gmail.com <using6843@gmail.com>
2024-11-14 16:18:11 +0900

    [chore] 스타일 조정

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -23,7 +23,7 @@ const Layout: React.FC<LayoutProps> = ({ path, children }) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{duration: 0.8}}
+        transition={{ duration: 0.8 }}
       >
         {children}
       </motion.div>
