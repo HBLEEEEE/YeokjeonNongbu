501ed4725a513dad2bb5d15a5e966c849c2dbee5
DongKey777 <higgs95@naver.com>
2024-11-21 19:08:40 +0900

    [feat] 계정 잔고 조회 API 추가

---
+++ apps/backend/src/account/account.repository.ts
@@ -0,0 +1,26 @@
+import { DatabaseService } from '../database/database.service';
+import { Injectable } from '@nestjs/common';
+import { AccountCashDto } from './dto/accountMoney.dto';
+
+@Injectable()
+export class AccountRepository {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async getCashFromMemberId(memberId: number): Promise<AccountCashDto> {
+    const query = `
+        SELECT available_cash, pending_cash, total_cash
+        FROM members
+        WHERE member_id = $1
+    `;
+
+    const values = [memberId];
+
+    const result = await this.databaseService.query(query, values);
+    return {
+      memberId: memberId,
+      availableCash: result.rows[0].available_cash,
+      pendingCash: result.rows[0].pending_cash,
+      totalCash: result.rows[0].total_cash
+    };
+  }
+}
