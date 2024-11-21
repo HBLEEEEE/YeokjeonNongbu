8f309250fd65efc5427dff7f830064acac01f472
HBLEEEEE <lhbs0528@gmail.com>
2024-11-21 16:45:56 +0900

    [fix] jwtSecret 환경변수화

--- apps/backend/src/mail/mail.module.ts
+++ apps/backend/src/mail/mail.module.ts
@@ -4,14 +4,19 @@ import { MailService } from './mail.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     DatabaseModule,
     EventEmitterModule.forRoot(),
-    JwtModule.register({
-      secret: 'web13',
-      signOptions: { expiresIn: '1h' }
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' }
+      })
     })
   ],
   controllers: [MailController],
