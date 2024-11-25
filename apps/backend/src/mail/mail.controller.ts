206fef4c90eb8764eb81820f474071110b9614b4
HBLEEEEE <lhbs0528@gmail.com>
2024-11-26 00:29:39 +0900

    [fix] 알림 기능 Redis pubsub으로 변경

--- apps/backend/src/mail/mail.controller.ts
+++ apps/backend/src/mail/mail.controller.ts
@@ -12,6 +12,64 @@ import { User } from 'src/global/utils/memberData';
 @Controller('api/mail')
 @ApiBearerAuth()
 export class MailController {
+  // private publisher: any;
+  // private subscriber: any;
+  // private clientSubjects = new Map<string, BehaviorSubject<string>>();
+
+  // async onModuleInit() {
+  //   // Redis Publisher와 Subscriber 초기화
+  //   this.publisher = createClient({ url: 'redis://localhost:6379' });
+  //   this.subscriber = createClient({ url: 'redis://localhost:6379' });
+
+  //   await this.publisher.connect();
+  //   await this.subscriber.connect();
+
+  //   // Redis Pub/Sub 메시지 처리
+  //   await this.subscriber.subscribe('server-events', (message: string) => {
+  //     const { clientId, data } = JSON.parse(message);
+  //     const subject = this.clientSubjects.get(clientId);
+
+  //     if (subject) {
+  //       subject.next(data); // 해당 클라이언트로 데이터 전달
+  //     }
+  //   });
+  // }
+
+  // @Sse('connect/:clientId')
+  // async connect(@Param('clientId') clientId: string) {
+  //   // 클라이언트별 BehaviorSubject 생성 및 관리
+  //   const subject = new BehaviorSubject<string>('Connected to server');
+  //   this.clientSubjects.set(clientId, subject);
+
+  //   // Redis에 클라이언트 정보 등록
+  //   const serverInfo = `http://localhost:3000`; // 현재 서버 정보
+  //   await this.publisher.set(`client:${clientId}`, serverInfo);
+
+  //   return subject.asObservable().pipe(map(message => ({ data: message }))); // 클라이언트로 Subject 반환
+  // }
+
+  // @Post('send')
+  // async sendMessage(@Body() body: { clientId: string; data: any }) {
+  //   const { clientId, data } = body;
+
+  //   // Redis에서 클라이언트가 연결된 서버 조회
+  //   const serverInfo = await this.publisher.get(`client:${clientId}`);
+  //   if (!serverInfo) {
+  //     throw new Error('Client not found in Redis');
+  //   }
+
+  //   if (serverInfo === 'http://localhost:3000') {
+  //     // 현재 서버에 연결된 경우
+  //     const subject = this.clientSubjects.get(clientId);
+  //     if (subject) {
+  //       subject.next(data); // 즉시 클라이언트로 데이터 전송
+  //     }
+  //   } else {
+  //     // 다른 서버에 연결된 경우 Pub/Sub을 통해 메시지 전달
+  //     await this.publisher.publish('server-events', JSON.stringify({ clientId, data }));
+  //   }
+  // }
+
   constructor(
     private readonly mailService: MailService,
     private readonly eventEmitter: EventEmitter2
--- apps/backend/src/mail/mail.service.ts
+++ apps/backend/src/mail/mail.service.ts
@@ -1,4 +1,10 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit
+} from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { Response } from 'express';
 import { DatabaseService } from 'src/database/database.service';
@@ -6,14 +12,105 @@ import { mailQueries } from './mail.queries';
 import { successhandler, successMessage } from 'src/global/successhandler';
 import { map, BehaviorSubject } from 'rxjs';
 import { MailRedisUtil } from './util/mailRedisUtil';
+import { createClient, RedisClientType } from 'redis';
+import { ConfigService } from '@nestjs/config';
+import * as os from 'os';
 
 @Injectable()
-export class MailService {
+export class MailService implements OnModuleInit, OnModuleDestroy {
   constructor(
     private readonly databaseService: DatabaseService,
     private readonly mailRedisUtil: MailRedisUtil
   ) {}
+  private subscriber: RedisClientType;
+  private publisher: RedisClientType;
   private sseSubjects: Map<number, BehaviorSubject<string>> = new Map();
+  private configService: ConfigService;
+  private myIp: string;
+
+  async onModuleInit() {
+    this.subscriber = createClient({ url: this.configService.get<string>('REDIS_URL') });
+    this.publisher = createClient({ url: this.configService.get<string>('REDIS_URL') });
+
+    await this.subscriber.connect();
+    await this.publisher.connect();
+
+    const networkInterfaces = os.networkInterfaces();
+    for (const interfaceName in networkInterfaces) {
+      const networkInfo = networkInterfaces[interfaceName];
+      if (networkInfo) {
+        const ipv4 = networkInfo.find(info => info.family === 'IPv4' && !info.internal);
+        if (ipv4) {
+          this.myIp = String(ipv4.address);
+          break;
+        }
+      }
+    }
+
+    this.subscriber.subscribe('notifications', message => {
+      const parsedMessage = JSON.parse(message);
+      const clientId = parsedMessage.clientId;
+      const data = parsedMessage.data;
+
+      if (this.sseSubjects.has(clientId)) {
+        this.sseSubjects.get(clientId)?.next(data);
+      }
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.subscriber.quit();
+    await this.publisher.quit();
+  }
+
+  async addClient(memberId: number, res: Response) {
+    const checkUnread = await this.databaseService.query(mailQueries.checkUnreadQuery, [memberId]);
+    const data = {
+      check: checkUnread.rows[0].result,
+      time: new Date()
+    };
+    const body = successhandler(successMessage.GET_MAIL_ALARM_SUCCESS, data);
+
+    if (!this.sseSubjects.has(memberId)) {
+      const newSubject = new BehaviorSubject<string>(JSON.stringify(body));
+      this.sseSubjects.set(memberId, newSubject);
+    }
+
+    const userSubject = this.sseSubjects.get(memberId);
+    if (!userSubject) {
+      throw new HttpException(
+        '유저 서브젝트가 제대로 생성되지 않았습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+
+    await this.publisher.set(`member:${memberId}`, this.myIp);
+
+    res.on('close', () => {
+      this.sseSubjects.delete(memberId);
+      this.mailRedisUtil.deleteSseRedis(String(memberId));
+      res.end();
+    });
+
+    return userSubject.asObservable().pipe(map(message => ({ data: message })));
+  }
+
+  async sendMessage(memberId: number, msg: string) {
+    const serverInfo = await this.publisher.get(`member:${memberId}`);
+    if (!serverInfo) {
+      console.log(`${memberId}번 유저에 대해서 알림을 보낼 수 없어요. 연결이 안됐거등요.`);
+      return;
+    }
+
+    if (serverInfo === this.myIp) {
+      const subject = this.sseSubjects.get(memberId);
+      if (subject) {
+        subject.next(msg);
+      }
+    } else {
+      await this.publisher.publich('server-events', JSON.stringify({ memberId, msg }));
+    }
+  }
 
   async connectSseAndInitiate(memberId: number, res: Response) {
     const checkUnread = await this.databaseService.query(mailQueries.checkUnreadQuery, [memberId]);
