73ed9d4e9f0bd32e2e4e5aad6718c3e8683a466a
HBLEEEEE <lhbs0528@gmail.com>
2024-11-26 10:32:42 +0900

    [feat] 알림 세분화 진행 중

---
+++ apps/backend/src/mail/util/mailCreateUtil.ts
@@ -0,0 +1,21 @@
+// export class MailCreateUtil{
+//   constructor(){}
+
+//   async createMailString(action:number, pram1:string, pram2:string, pram3:string, text:string ) {
+//     if(action==1){
+//       return `${pram1}을(를) ${pram2}원에 ${pram3}개 매수했습니다.`
+//     } else if(action==2){
+//       return `${pram1}을(를) ${pram2}원에 ${pram3}개 매도했습니다.`
+//     } else if(action==3){
+//       return `${text}`
+//     } else if(action==4){
+//       return `${pram1}님이 복권을 구매하여 ${pram2}등에 당첨됐습니다.`
+//     } else if(action==5){
+//       return `${pram1}님께서 당신의 농장에 방문하셨습니다.`
+//     } else if(action==6){
+//       return `${pram1}등을 달성하셨습니다.`
+//     } else if(action==2){
+//       return `${pram1}님께서 총 자산 ${pram2}원에 도달하셨습니다.`
+//     }
+//   }
+// }
