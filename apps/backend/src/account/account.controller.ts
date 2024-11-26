83effd7525c6a93746dd80a019339eb8c97c2489
DongKey777 <higgs95@naver.com>
2024-11-26 11:46:54 +0900

    [refactor] 거래소 api oauth 적용

--- apps/backend/src/account/account.controller.ts
+++ apps/backend/src/account/account.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { accountCashDecorator } from './decorator/account.decorator';
 import { successhandler, successMessage } from '../global/successhandler';
+import { JwtAuthGuard } from '../global/utils/jwtAuthGuard';
+import { User } from '../global/utils/memberData';
 
 @Controller('api/account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
   @Get('cash')
+  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: '회원의 현금 정보 조회' })
   @accountCashDecorator()
-  async getCashFromMemberId(@Query('memberId') memberId: number) {
+  async getCashFromMemberId(@User() user: { memberId: number }) {
+    const { memberId } = user;
     const cash = await this.accountService.getCashFromMemberId(memberId);
     return successhandler(successMessage.GET_ACCOUNT_CASH_SUCCESS, cash);
   }
