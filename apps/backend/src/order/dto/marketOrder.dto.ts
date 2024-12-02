0717bee8b7aaf255805bf1b085fd202b4545eea6
DongKey777 <higgs95@naver.com>
2024-12-02 14:48:49 +0900

    [refactor] 주문 취소 API 에러처리

--- apps/backend/src/order/dto/marketOrder.dto.ts
+++ apps/backend/src/order/dto/marketOrder.dto.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { BaseOrderDto } from './baseOrder.dto';
+import { IsInt } from 'class-validator';
 
 export class MarketOrderDto extends BaseOrderDto {
   @ApiProperty({ description: '총 주문 금액', example: 50000 })
+  @IsInt({ message: '총 주문 금액은 정수여야 합니다.' })
   totalAmount: number;
 
   @ApiProperty({ description: '주문 수량', example: 100 })
+  @IsInt({ message: '수량은 정수여야 합니다.' })
   quantity: number | null;
 }
