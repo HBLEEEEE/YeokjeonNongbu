dfc680b4de8777c55b7eed873bab16f9a516c2f3
DongKey777 <higgs95@naver.com>
2024-12-02 15:18:55 +0900

    [refactor] 시장가 주문 검증 로직 수정

--- apps/backend/src/order/dto/marketOrder.dto.ts
+++ apps/backend/src/order/dto/marketOrder.dto.ts
@@ -1,10 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { BaseOrderDto } from './baseOrder.dto';
-import { IsInt } from 'class-validator';
 
 export class MarketOrderDto extends BaseOrderDto {
   @ApiProperty({ description: '총 주문 금액', example: 50000 })
-  @IsInt({ message: '총 주문 금액은 정수여야 합니다.' })
   totalAmount: number;
 
   @ApiProperty({ description: '주문 수량', example: 100 })
